ee41396ae0b506c2a787923a3f3cb284feae9b14
bad alx <o.badalx@googlemail.com>
2016-03-22 17:58:12 +0100

    Engine Defines nicht im Namespace

--- triebWerk/include/EngineDefines.h
+++ triebWerk/include/EngineDefines.h
@@ -1,5 +1,5 @@
 #pragma once
-#define Engine triebWerk::CEngine::Instance()
-#define Input triebWerk::CEngine::Instance().m_pInput
-#define Time triebWerk::CEngine::Instance().m_pTime
-#define World triebWerk::CEngine::Instance().m_pWorld
+#define Engine CEngine::Instance()
+#define Input CEngine::Instance().m_pInput
+#define Time CEngine::Instance().m_pTime
+#define World CEngine::Instance().m_pWorld
