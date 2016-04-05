135b17b61f68c48b606cdaa725b5793ab946b1b5
bad alx <o.badalx@googlemail.com>
2016-04-05 20:27:13 +0200

    Improved Window Class - change fullscreen and resolution on runtime

--- triebWerk/include/CWindow.h
+++ triebWerk/include/CWindow.h
@@ -8,22 +8,32 @@ namespace triebWerk
 	public:
 		CWindow();
 		~CWindow();
-
 	public:
-		const bool Initialize(const bool a_IsFullscreen, const unsigned short a_ScreenWidth, const unsigned short a_ScreenHeight, const char* a_WindowName);
+		bool Initialize(const bool a_IsFullscreen, const unsigned short a_ScreenWidth, const unsigned short a_ScreenHeight, const char* a_WindowName);
 		
 		const MSG GetWindowEvent();
 
 		static LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
+		void ChangeWindowSettings(const bool a_IsFullscreen, const unsigned short a_ScreenWidth, const unsigned short a_ScreenHeight);
+		
 		HWND* GetModulHanle();
 
 		const unsigned short GetScreenWidth();
 		const unsigned short GetScreenHeight();
+		
+		bool IsWindowFullscreen();
+
+		static int GetMaximalDisplayWidth();
+		static int GetMaximalDisplayHeight();
 
 	private:
 		HWND m_WindowHandle;
+
 		unsigned short m_Width;
 		unsigned short m_Height;
+
+		bool m_IsFullscreen;
+		bool m_ShowCursor;
 	};
 }
--- triebWerk/src/CWindow.cpp
+++ triebWerk/src/CWindow.cpp
@@ -1,8 +1,13 @@
+#include <CWindow.h>
 #include <Windows.h>
-#include "..\include\CWindow.h"
 #include <iostream>
 
-triebWerk::CWindow::CWindow()
+triebWerk::CWindow::CWindow() : 
+	m_Height(0),
+	m_Width(0),
+	m_IsFullscreen(false),
+	m_ShowCursor(true),
+	m_WindowHandle(NULL)
 {
 }
 
@@ -10,95 +15,52 @@ triebWerk::CWindow::~CWindow()
 {
 }
 
-const bool triebWerk::CWindow::Initialize(const bool a_IsFullscreen, const unsigned short a_ScreenWidth, const unsigned short a_ScreenHeight, const char* a_WindowName)
+bool triebWerk::CWindow::Initialize(const bool a_IsFullscreen, const unsigned short a_ScreenWidth, const unsigned short a_ScreenHeight, const char* a_WindowName)
 {
+	//Set initialize values
+	m_Height = a_ScreenHeight;
+	m_Width = a_ScreenWidth;
+	m_IsFullscreen = a_IsFullscreen;
+
+	//window sytle
+	WNDCLASSEX mainWindowDescription;
+	ZeroMemory(&mainWindowDescription, sizeof(WNDCLASSEX));
+	mainWindowDescription.cbSize = sizeof(WNDCLASSEX);
+	mainWindowDescription.style = CS_HREDRAW | CS_VREDRAW;
+	mainWindowDescription.lpfnWndProc = WindowProcedure;
+	mainWindowDescription.hInstance = GetModuleHandle(NULL);
+	mainWindowDescription.hCursor = LoadCursor(NULL, IDC_ARROW);
+	mainWindowDescription.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	mainWindowDescription.lpszClassName = a_WindowName;
+
+	RegisterClassEx(&mainWindowDescription);
+
+	RECT windowRectangle = { 0, 0, a_ScreenWidth ,a_ScreenHeight };
+	AdjustWindowRect(&windowRectangle, WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX, FALSE);
+
+	//Create window 
+	m_WindowHandle = CreateWindowEx(NULL,
+		a_WindowName,
+		a_WindowName,
+		WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
+		0,
+		0,
+		windowRectangle.right - windowRectangle.left,
+		windowRectangle.bottom - windowRectangle.top,
+		NULL,
+		NULL,
+		GetModuleHandle(NULL),
+		NULL);
+
+	if (m_WindowHandle == NULL)
+		return false;
+
+	ShowWindow(m_WindowHandle, SW_SHOWDEFAULT);
+	m_ShowCursor = true;
+
+	//If fullscreen change normal window above to fullscreen
 	if (a_IsFullscreen)
-	{
-		WNDCLASSEX wc;
-		DEVMODE dmScreenSettings;
-		int posX, posY;
-		HMODULE hModule;
-
-		hModule = GetModuleHandle(NULL);
-
-		wc.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
-		wc.lpfnWndProc = WindowProcedure;
-		wc.cbClsExtra = 0;
-		wc.cbWndExtra = 0;
-		wc.hInstance = hModule;
-		wc.hIcon = LoadIcon(NULL, IDI_WINLOGO);
-		wc.hIconSm = wc.hIcon;
-		wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-		wc.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
-		wc.lpszMenuName = NULL;
-		wc.lpszClassName = a_WindowName;
-		wc.cbSize = sizeof(WNDCLASSEX);
-
-		RegisterClassEx(&wc);
-
-		dmScreenSettings.dmSize = sizeof(dmScreenSettings);
-		dmScreenSettings.dmPelsWidth = GetSystemMetrics(SM_CXSCREEN);
-		dmScreenSettings.dmPelsHeight = GetSystemMetrics(SM_CYSCREEN);
-		dmScreenSettings.dmBitsPerPel = 32;
-		dmScreenSettings.dmFields = DM_BITSPERPEL | DM_PELSWIDTH | DM_PELSHEIGHT;
-
-		m_Width = static_cast<unsigned short>(dmScreenSettings.dmPelsWidth);
-		m_Height = static_cast<unsigned short>(dmScreenSettings.dmPelsHeight);
-
-		ChangeDisplaySettings(&dmScreenSettings, CDS_FULLSCREEN);
-
-		posX = posY = 0;
-
-		m_WindowHandle = CreateWindowEx(WS_EX_APPWINDOW, a_WindowName, a_WindowName,
-			WS_CLIPSIBLINGS | WS_CLIPCHILDREN | WS_POPUP,
-			posX, posY, a_ScreenWidth, a_ScreenHeight, NULL, NULL, hModule, NULL);
-
-		ShowWindow(m_WindowHandle, SW_SHOW);
-		SetForegroundWindow(m_WindowHandle);
-		SetFocus(m_WindowHandle);
-
-		ShowCursor(false);
-	}
-	else
-	{
-		WNDCLASSEX mainWindowDescription;
-
-		memset(&mainWindowDescription, 0, sizeof(WNDCLASSEX));
-		mainWindowDescription.cbSize = sizeof(WNDCLASSEX);
-		mainWindowDescription.style = CS_HREDRAW | CS_VREDRAW;
-		mainWindowDescription.lpfnWndProc = WindowProcedure;
-		mainWindowDescription.hInstance = GetModuleHandle(NULL);
-		mainWindowDescription.hCursor = LoadCursor(NULL, IDC_ARROW);
-		mainWindowDescription.hbrBackground = (HBRUSH)COLOR_WINDOW;
-		mainWindowDescription.lpszClassName = a_WindowName;
-
-		RegisterClassEx(&mainWindowDescription);
-
-		RECT windowRectangle = { 0, 0, a_ScreenWidth ,a_ScreenHeight };
-		AdjustWindowRect(&windowRectangle, WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX, FALSE);
-
-		m_Width = a_ScreenWidth;
-		m_Height = a_ScreenHeight;
-
-		m_WindowHandle = CreateWindowEx(NULL,
-			a_WindowName,
-			a_WindowName,
-			WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
-			100,
-			100,
-			windowRectangle.right - windowRectangle.left,
-			windowRectangle.bottom - windowRectangle.top,
-			NULL,
-			NULL,
-			GetModuleHandle(NULL),
-			NULL);
-
-
-		if (m_WindowHandle == NULL)
-			return false;
-
-		ShowWindow(m_WindowHandle, SW_SHOWDEFAULT);
-	}
+		ChangeWindowSettings(true, m_Width, m_Height);
 
 	return true;
 }
@@ -143,4 +105,79 @@ const unsigned short triebWerk::CWindow::GetScreenWidth()
 const unsigned short triebWerk::CWindow::GetScreenHeight()
 {
 	return m_Height;
+}
+
+bool triebWerk::CWindow::IsWindowFullscreen()
+{
+	return m_IsFullscreen;
+}
+
+int triebWerk::CWindow::GetMaximalDisplayWidth()
+{
+	DEVMODE dmScreenSettings;
+	EnumDisplaySettings(NULL, 0, &dmScreenSettings);
+	return dmScreenSettings.dmPelsWidth;
+}
+
+int triebWerk::CWindow::GetMaximalDisplayHeight()
+{
+	DEVMODE dmScreenSettings;
+	EnumDisplaySettings(NULL, 0, &dmScreenSettings);
+	return dmScreenSettings.dmPelsHeight;
+}
+
+void triebWerk::CWindow::ChangeWindowSettings(const bool a_IsFullscreen, const unsigned short a_ScreenWidth, const unsigned short a_ScreenHeight)
+{
+	//Set new values
+	m_IsFullscreen = a_IsFullscreen;
+	m_Width = a_ScreenWidth;
+	m_Height = a_ScreenHeight;
+
+	//Set window rect
+	RECT windowRectangle = { 0, 0, a_ScreenWidth ,a_ScreenHeight };
+
+	DEVMODE dmScreenSettings;
+	EnumDisplaySettings(NULL, 0, &dmScreenSettings);
+	dmScreenSettings.dmSize = sizeof(dmScreenSettings);
+
+	//If Fullscreen set target resolution if not set "normal" resolution
+	if (a_IsFullscreen)
+	{
+		dmScreenSettings.dmPelsWidth = a_ScreenWidth;
+		dmScreenSettings.dmPelsHeight = a_ScreenHeight;
+	}
+	else
+	{
+		dmScreenSettings.dmPelsWidth = static_cast<DWORD>(GetMaximalDisplayWidth());
+		dmScreenSettings.dmPelsHeight = static_cast<DWORD>(GetMaximalDisplayHeight());
+	}
+	dmScreenSettings.dmBitsPerPel = 32;
+	dmScreenSettings.dmFields = DM_BITSPERPEL | DM_PELSWIDTH | DM_PELSHEIGHT;
+
+	ChangeDisplaySettingsEx(NULL, &dmScreenSettings, NULL, CDS_FULLSCREEN, NULL);
+
+	//Set the style for fullscreen or window
+	if(a_IsFullscreen)
+		SetWindowLongPtr(m_WindowHandle, GWL_STYLE, WS_CLIPSIBLINGS | WS_CLIPCHILDREN | WS_POPUP);
+	else
+		SetWindowLongPtr(m_WindowHandle, GWL_STYLE, WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX);
+
+	//If window calculate correct client space
+	if (!a_IsFullscreen)
+		AdjustWindowRect(&windowRectangle, WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX, FALSE);
+
+	//Resize the window and draw it new
+	SetWindowPos(m_WindowHandle, NULL, 0, 0, windowRectangle.right - windowRectangle.left, windowRectangle.bottom - windowRectangle.top, SWP_FRAMECHANGED | SWP_NOZORDER | SWP_NOOWNERZORDER | SWP_SHOWWINDOW);
+
+	//Disable Cursor if Fullscreen enable if window
+	if (m_ShowCursor == true && a_IsFullscreen)
+	{
+		ShowCursor(false);
+		m_ShowCursor = false;
+	}
+	else if(m_ShowCursor == false && !a_IsFullscreen)
+	{
+		ShowCursor(true);
+		m_ShowCursor = true;
+	}
 }
