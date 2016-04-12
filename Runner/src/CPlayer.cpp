c5f77382b93c7d994e837f42335844f2eeb4baec
Manuel Härtl <manuelhaertl@googlemail.com>
2016-04-12 20:14:49 +0200

    player

--- Runner/src/CPlayer.cpp
+++ Runner/src/CPlayer.cpp
@@ -18,11 +18,11 @@ void CPlayer::Update()
     if (twKeyboard.IsState(triebWerk::EKey::Space, triebWerk::EButtonState::Down))
         std::cout << "Keyboard Space" << std::endl;
 
-    if (twMouse.IsState(triebWerk::EMouseButton::Left, triebWerk::EButtonState::Down))
-        std::cout << "Mouse Left" << std::endl;
-
     if (twGamepad.IsState(triebWerk::EGamepadButton::A, triebWerk::EButtonState::Down, 0))
         std::cout << "Gamepad A" << std::endl;
+
+    if (twMouse.IsState(triebWerk::EMouseButton::Left, triebWerk::EButtonState::Down))
+        std::cout << "Mouse Left" << std::endl;
 }
 
 void CPlayer::End()
